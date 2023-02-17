43dc1645ca6773992d57d47e0c77f740f45c297b
Tom Joshi-Cale <tj294@exeter.ac.uk>
2023-02-17 16:04:35 +0000

    added style

--- api/main.py
+++ api/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from . import settings
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name='static')
 
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
-    return settings.TEMPLATES.TemplateResponse("homepage.html", {"request": request})
+    return settings.TEMPLATES.TemplateResponse("homepage.html", {"request": request, "name": 'World'})
 
 @app.get("/hello")
 async def hello():
     return f"Hello, world!"
 
 @app.get("/hello/{name}")
-async def hello_name(name: str):
-    return f"Hello, {name}!"
+async def hello_name(request: Request, name: str):
+    return settings.TEMPLATES.TemplateResponse("homepage.html", {"request": request, "name": name})
