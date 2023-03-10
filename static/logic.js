6142b44667d794daebdb6a569a276dd3f0f5676f
Tom Joshi-Cale <tj294@exeter.ac.uk>
2023-03-10 14:02:25 +0000

    Mandelbrot Set Website

--- static/logic.js
+++ static/logic.js
@@ -1,21 +1,36 @@
 function sample() {
-    const x = 3;
+    const real = document.getElementById("realInput").value;
+    const imag = document.getElementById("imagInput").value;
 
-    const url = "http://localhost:8000/sample";
+    const width = document.getElementById('widthInput').value;
+    const height = document.getElementById('heightInput').value;
+
+    const zoom = document.getElementById("zoomInput").value;
+    const maxIter = document.getElementById("maxIterInput").value;
+
+    const url = "http://localhost:8000/image";
 
     const params = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ x: x })
+        body: JSON.stringify({ 
+            real: real,
+            imag: imag,
+            width: width,
+            height: height,
+            zoom: zoom,
+            max_iters: maxIter,
+        })
     }
 
-    fetch(url, params).then((response) => {
-        console.log(response);
-        // console.log(response.text().then(console.log))
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+    fetch(url, params)
+        .then((_response) => {
+            document.body.style.backgroundImage = "url(/static/mandy.png?r=" + Math.random() + ")";
+        })
+        .catch((error => {
+            console.log(error);
+        }));
+s
 }
