3499b1364d9e4c54f10c2074b316261fae5b4a1a
Tom Joshi-Cale <tj294@exeter.ac.uk>
2023-03-10 14:02:05 +0000

    Mandelbrot Set Website

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, Response
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-
+import mandybrot as mandy
 from . import settings
 
 app = FastAPI()
@@ -31,8 +31,30 @@ async def hello_name(request: Request, name: str):
     return settings.TEMPLATES.TemplateResponse("homepage.html", {"request": request, "name": name})
 
 class SampleInput(BaseModel):
-    x: int
+    real: float
+    imag: float
 
 @app.post("/sample")
 async def sample(input: SampleInput):
-    return input.x**2
+    c = input.real + input.imag * 1j
+    max_iters = 100
+    return mandy.sample(c, max_iters)
+
+class ImageInput(BaseModel):
+    real: float
+    imag: float
+    width: int
+    height: int
+    zoom: float
+    max_iters: int
+
+@app.post("/image")
+async def image(input: ImageInput):
+    data = mandy.sample.area(
+        input.real, input.imag, input.width, input.height, input.zoom, input.max_iters
+    )
+    cols = mandy.colour.image(data, input.max_iters, mandy.colour.jet_map)
+    img = mandy.colour.encode(cols)
+    img.save("static/mandy.png")
+
+    return "Done"
